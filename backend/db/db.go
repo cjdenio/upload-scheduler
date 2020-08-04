49ba071de67a27638a30ccc8a96170d763eb95a0
Caleb Denio <cjdenio44@gmail.com>
2020-08-04 16:28:15 -0400

    Add basic database

---
+++ backend/db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// DB is the MongoDB client instance
+var DB *mongo.Client
+
+// Connect to the DB
+func Connect() {
+	uri := "mongodb://db:27017/upload-scheduler"
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	DB = client
+}
